25938ca77e65352d56038edd0f7f4bb23a18f946
Pavel Raiskup <praiskup@redhat.com>
2015-05-20 00:19:23 +0200

    testsuite: add test-case for dockerfile generation
    Test that project.py together with docker.tpl & spec.yaml works
    correctly.
    
    * tests/testsuite: Also specify --projectdir while calling dg.
    * tests/dockerfile/distros: New file.
    * tests/dockerfile/project.py: Likewise.
    * tests/dockerfile/test.exp: Likewise.
    * tests/dockerfile/test.tpl: Likewise.
    * tests/dockerfile/test.yaml: Likewise.

---
+++ tests/dockerfile/project.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+from distgen.project import AbstractProject
+
+class Project(AbstractProject):
+
+    maintainer = "Pavel Raiskup <praiskup@redhat.com>"
+
+    def inst_init(self, spec, template, sysconf):
+        """
+        Demonstrate logic in inst_init().
+        """
+        test_var = "{0}-{1}-{2}".format(sysconf['os']['id'],
+                                        sysconf['os']['version'],
+                                        sysconf['os']['arch'])
+
+        # Returned dict may be reused directly in yaml specification
+        return {'test_var': "build_for {0}".format(test_var)}
+
+    def inst_finish(self, spec, tpl, config, data):
+
+        # We can in-place update 'data' variable generated from yaml
+        # specification.
+        if config['os']['id'] == 'fedora':
+            action = {'type': 'pkg'}
+            action['action'] = "install"
+            action['packages'] = ['fedpkg']
+
+            data['parts']['pkginstall']['data'].append(action)
