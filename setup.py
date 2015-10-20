267c3cd30d7d9b9419400ebcf864f88ae6e3c9c2
Pavel Raiskup <praiskup@redhat.com>
2015-10-20 17:42:18 +0200

    distconf: add rawhide (and fc24)
    From now, MANIFEST.in covers distconf files distribution.
    
    Resolves: devexp-db/distgen/issues/3
    
    * MANIFEST.in: New patterns for distconf.
    * distconf/fedora-24-i686.yaml: New file.
    * distconf/fedora-24-x86_64.yaml: Likewise.
    * distconf/fedora-rawhide-i686.yaml: New symlink.
    * distconf/fedora-rawhide-x86_64.yaml: Likewise.
    * setup.py: Don't distribute distconf here.

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ datadir = "share"
 pkgdatadir = datadir + "/" + project
 tpldir = pkgdatadir + "/templates"
 
-distconfdir = pkgdatadir + "/distconf"
-
 setup(
     name='distgen',
     version=dg_version,
@@ -25,23 +23,6 @@ setup(
             'templates/makefile-macros.tpl',
             'templates/README',
             'templates/general.tpl',
-        ]),
-        (distconfdir, [
-            'distconf/fedora-20-i686.yaml',
-            'distconf/fedora-20-x86_64.yaml',
-            'distconf/fedora-21-i686.yaml',
-            'distconf/fedora-21-x86_64.yaml',
-            'distconf/fedora-22-i686.yaml',
-            'distconf/fedora-22-x86_64.yaml',
-            'distconf/fedora-23-i686.yaml',
-            'distconf/fedora-23-x86_64.yaml',
-            'distconf/rhel-7-x86_64.yaml',
-        ]),
-        (distconfdir + "/lib", [
-            'distconf/lib/fedora.yaml',
-            'distconf/lib/general.yaml',
-            'distconf/lib/rhel.yaml',
-            'distconf/lib/rpmsystems.yaml',
         ]),
     ],
     scripts=['bin/dg'],
