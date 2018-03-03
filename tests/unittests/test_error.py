177d5ec1d464b34dde26bdd6e705b26266871f54
Pavel Raiskup <praiskup@redhat.com>
2018-03-03 09:47:32 +0100

    tests: fix CI for test_error()
    Seems like capsys has some problems to catch logging output
    through stderr, so use pytest-catchlog.

--- tests/unittests/test_error.py
+++ tests/unittests/test_error.py
@@ -1,11 +1,12 @@
 import pytest
+import logging
 
 from distgen.err import fatal
 
 
 class TestFatal(object):
-    def test_fatal(self, capsys):
+    def test_fatal(self, capsys, caplog):
         with pytest.raises(SystemExit) as e:
             fatal('Message')
-        assert capsys.readouterr()[1] == 'CRITICAL:root:Message\n'
+        assert caplog.record_tuples == [('root', logging.CRITICAL, 'Message')]
         assert e.value.code == 1
