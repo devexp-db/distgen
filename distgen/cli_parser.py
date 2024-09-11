9ffb96dbb372926e59a610bf645714d161ac9a69
Ales Nezbeda <anezbeda@redhat.com>
2024-09-11 17:19:29 +0200

    Add support for automatic manpage generation

--- distgen/cli_parser.py
+++ distgen/cli_parser.py
@@ -1,27 +1,31 @@
-"""
-Separated argument parser.  This makes the manual page generator happy because
-we don't need any other dependencies installed at build-time.  See #149.
-"""
-
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-from distgen.version import dg_version
+from importlib.metadata import version, PackageNotFoundError
 
-DESCRIPTION = """\
+try:
+    version = version("distgen")
+except PackageNotFoundError:
+    # package is not installed, due to this file being used by manpage generator
+    # we have to check for this, we do not need valid version during manpage
+    # generation, but it will crash
+    version = 0
+    pass
+
+description = \
+    """
 Generate script using predefined metadata about distribution and
 templates.
 
 As an example of 'dg' usage, to generate _Dockerfile_ for Fedora
 21 64-bit system, you may use command(s):
 
- $ cd project/directory
- $ dg --spec      docker-data.yaml      \\
-      --template  docker.tpl
-"""
+$ cd project/directory
+$ dg --spec      docker-data.yaml      \\
+--template  docker.tpl
+    """
 
 parser = ArgumentParser(
-    prog='dg',
-    description=DESCRIPTION,
+    description=description,
     formatter_class=RawDescriptionHelpFormatter,
 )
 
@@ -31,7 +35,7 @@ parser.man_short_description = "templating system/generator for distributions"
 parser.add_argument(
     '--version',
     action='version',
-    version=f"dg (distgen) {dg_version}",
+    version="dg (distgen) {0}".format(version)
 )
 
 parser.add_argument(
